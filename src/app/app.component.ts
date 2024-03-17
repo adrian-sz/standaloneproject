2ef0b701c692ff49ebad7273153c296d74d1a053
adrian-sz <adrianszczesek@gmail.com>
2024-03-17 14:55:23 +0100

    implementation of menu component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CharacterStatsComponent } from './character-stats/character-stats.component';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    CharacterStatsComponent
+    CharacterStatsComponent,
+    MenuComponent
   ],
   //templateUrl: './app.component.html',
   template: `
   <main>
+    
     <header class="brand-name">
       <img class="brand-logo" src="/assets/supersus.png" alt="logo" aria-hidden="true">
     </header>
-    <section class="content">
+
+    <section class="menu">
+      <app-menu></app-menu>
+    </section>
+    
+    <section class="characterstats">
       <app-character-stats></app-character-stats>
     </section>
+
   </main>
 `,
   styleUrl: './app.component.css'
