ea1dcaed9095cf2302b53ca7d3231d1b3af64560
adrian-sz <adrianszczesek@gmail.com>
2024-03-17 14:13:25 +0100

    implementing character-stats onto a new empty project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CharacterStatsComponent } from './character-stats/character-stats.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [
+    RouterOutlet,
+    CharacterStatsComponent
+  ],
+  //templateUrl: './app.component.html',
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-character-stats></app-character-stats>
+    </section>
+  </main>
+`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
---
+++ src/app/character-stats/character-stats.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-stats',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      character-stats works!
+    </p>
+  `,
+  styleUrl: './character-stats.component.css'
+})
+export class CharacterStatsComponent {
+
+}
