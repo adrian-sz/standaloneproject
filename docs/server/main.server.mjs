231f08bac912d4df7d9e77ea14dc49ec250e9c2a
adrian-sz <adrianszczesek@gmail.com>
2024-03-22 22:41:49 +0100

    built for pages

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BZWKFWLX.mjs";import"./chunk-PJHKHHI6.mjs";import"./chunk-6GSYOFKM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-LJXUAGKK.mjs";import"./chunk-PJHKHHI6.mjs";import"./chunk-6GSYOFKM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
